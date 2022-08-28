e61944f438be0649d4ce82b2876f31b1aaa5a6c0
Mallikharjun12 <yedukondalu.n@anynews.co.in>
2022-08-28 15:32:22 +0530

    initial Xcode project creation,added launch screen

---
+++ Yummie/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  OnboardingViewController.swift
+//  Yummie
+//
+//  Created by admin on 26/08/22.
+//  Copyright © 2022 konda. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
